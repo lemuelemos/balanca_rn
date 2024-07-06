d12538491c862a9a97ed8991419e2e711ff17d32
lemuelemos <lemuelemos@gmail.com>
2024-07-06 19:51:22 -0300

    Initial commit

--- graficos.R
+++ graficos.R
@@ -1,4 +1,5 @@
 library(readr)
+library(dplyr)
 
 EXP_2024 <- read_delim("dados/EXP_2024.csv", 
                        delim = ";", 
@@ -27,4 +28,32 @@ NCM <- read_delim("dados/NCM.csv", delim = ";",
 
 bind_rows(EXP_2024,
           IMP_2024) %>% 
-  mutate(Competência = format(as.Date(paste0(CO_ANO,"-" , "-01")),"%B %Y"))
+  mutate(Competência = as.Date(paste0(CO_ANO,
+                                      "-",
+                                      CO_MES ,
+                                      "-01"))) %>% 
+  arrange(CO_ANO,CO_MES) %>% 
+  left_join(
+    NCM %>% 
+      select(CO_NCM,NO_NCM_POR)
+  ) %>% 
+  summarise(VL_FOB = sum(VL_FOB),.by = c("Competência","TIPO")) %>% 
+  tidyr::pivot_wider(names_from = TIPO,values_from = VL_FOB) %>% 
+  mutate(Saldo = EXP - IMP) %>% 
+  ggplot() +
+  labs(
+    title = "Balança Comercial do Rio Grande do Norte",
+    subtitle = "Evolução das Exportações/Importações no primeiro semestre de 2024",
+    caption = "Fonte: {palmerpenguins} traduzido no pacote {dados}"
+  ) +
+  geom_line(aes(Competência,Saldo)) +
+  geom_col(aes(Competência,EXP)) +
+  scale_x_date(date_labels = "%b-%Y") +
+  scale_y_continuous(labels = unit_format(unit = "M", scale = 1e-6)) +
+  tema_g1()
+
+
+
+
+
+
